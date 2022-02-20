b4e029c8e52b922b8bf91b7b3780598f63f00961
Snull <snull5011@hotmail.com>
2022-02-20 19:52:44 +0800

    Move tests to its own folder, and fix file_exists() bug.

--- src/common/filesystem.cpp
+++ src/common/filesystem.cpp
@@ -5,25 +5,6 @@
 namespace ln {
 
 bool
-file_exists(const std::string &i_path)
-{
-    if (i_path.empty())
-    {
-        return false;
-    }
-
-    if (auto file = std::fopen(i_path.c_str(), "rb"))
-    {
-        std::fclose(file);
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool
 file_rename(const std::string &i_old, const std::string &i_new)
 {
     return !std::rename(i_old.c_str(), i_new.c_str());
---
+++ src/common/filesystem_macos.cpp
@@ -0,0 +1,14 @@
+#include "filesystem.hpp"
+
+#include <sys/stat.h>
+
+namespace ln {
+
+bool
+file_exists(const std::string &i_path)
+{
+    struct stat st;
+    return (stat(i_path.c_str(), &st) == 0) && !(st.st_mode & S_IFDIR);
+}
+
+} // namespace ln
